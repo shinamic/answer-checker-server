58b68373f7052d8f5abb7db8466780f3d3e653f0
benhexie <benedictgabriel73@gmail.com>
2023-06-29 02:55:10 +0100

    Removed limit

--- routes/getQuestion.js
+++ routes/getQuestion.js
@@ -2,7 +2,7 @@ const pool = require('../connection/pool');
 
 module.exports = (req, res) => {
     // Fetch 5 questions from the questions table
-    pool.query('SELECT question, id FROM questions LIMIT 2', (error, results) => {
+    pool.query('SELECT question, id FROM questions', (error, results) => {
      if (error) {
        console.error('Error retrieving questions:', error);
        res.status(500).send('An error occurred');
